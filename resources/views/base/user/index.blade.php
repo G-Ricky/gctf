aaeb216109ffbe4f3af46a9b6a00f5903b63f06b
G-Ricky <284215049@qq.com>
2018-08-19 18:58:49 +0800

    添加前端操作

--- resources/views/base/user/index.blade.php
+++ resources/views/base/user/index.blade.php
@@ -7,8 +7,23 @@
 
 @section('content')
 <div class="ui container">
-    <h4 class="ui horizontal divider header"><i class="tag icon"></i> 修改个人资料 </h4>
-    <div class="ui form">
+    <div class="ui dimmer" id="global-loader">
+        <div class="ui big text loader"></div>
+    </div>
+    <div class="ui basic vertical clearing segment">
+        <div class="ui right floated main menu">
+            <a class="icon item" id="btn-edit" href="javascript:edit();">
+                <i class="edit icon"></i>&nbsp;<span>编辑</span>
+            </a>
+            <a class="icon item" id="btn-cancel" href="javascript:cancel();" style="display: none;">
+                <i class="edit icon"></i>&nbsp;<span>取消</span>
+            </a>
+            <a class="icon item disabled" id="btn-save" href="javascript:save();" style="display: none;">
+                <i class="edit icon"></i>&nbsp;<span>保存</span>
+            </a>
+        </div>
+    </div>
+    <div class="ui form" id="form">
         <div class="field">
             <label>学号</label>
             <input id="sid" type="text" value="" readonly>
@@ -23,7 +38,7 @@
         </div>
         <div class="field">
             <label>性别</label>
-            <select id="gender">
+            <select class="ui dropdown" id="gender" disabled>
                 <option value="UNKNOWN" selected>未知</option>
                 <option value="MALE">男</option>
                 <option value="FEMALE">女</option>
@@ -33,31 +48,132 @@
             <label>邮箱</label>
             <input id="email" type="text" value="" readonly>
         </div>
-        <div class="field">
-            <button class="ui submit button" disabled>保存</button>
-        </div>
     </div>
 </div>
 @endsection
 
 @push('scripts')
 <script>
-    $(document).ready(function() {
-        $.get(
-            "{{ url('user/info') }}",
-            function(data, status) {
-                if(status === "success") {
-                    if(data.success) {
-                        $("#sid").val(data.data.sid);
-                        $("#name").val(data.data.name);
-                        $("#nickname").val(data.data.nickname);
-                        $("#gender").find("option[text='" + data.data.gender + "']").attr("selected",true);
-                        $("#email").val(data.data.email);
-                    }
+    window.info = {
+        "sid"     : "",
+        "name"    : "",
+        "nickname": "",
+        "gender"  : "",
+        "email"   : ""
+    };
+    function fill(info) {
+        $("#sid").val(info.sid);
+        $("#name").val(info.name);
+        $("#nickname").val(info.nickname);
+        $("#gender").dropdown("clear").dropdown("set selected", info.gender);
+        $("#email").val(info.email);
+    }
+    function getInfo() {
+        return {
+            "sid"     : $("#sid").val(),
+            "name"    : $("#name").val(),
+            "nickname": $("#nickname").val(),
+            "gender"  : $("#gender").dropdown("get value"),
+            "email"   : $("#email").val()
+        };
+    }
+    function isSameInfo(info1, info2) {
+        return info1.sid === info2.sid &&
+            info1.name === info2.name &&
+            info1.nickname === info2.nickname &&
+            info1.gender === info2.gender &&
+            info1.email === info2.email;
+    }
+    function infoHasChanged() {
+        let newInfo = getInfo();
+        return !isSameInfo(window.info, newInfo);
+    }
+    function changeSaveButtonStatus() {
+        if(infoHasChanged()) {
+            $("#btn-save").removeClass("disabled");
+            $("#btn-save").attr("href", "javascript:save();");
+        }else{
+            $("#btn-save").attr("href", "javascript:void(0);");
+            $("#btn-save").addClass("disabled");
+        }
+    }
+    function openLoader(message = "") {
+        $("#global-loader").children(".loader").html(message);
+        $("#global-loader").addClass("active");
+    }
+    function closeLoader() {
+        $("#global-loader").removeClass("active");
+    }
+    function edit() {
+        $("#btn-edit").hide();
+        $("#btn-cancel").show();
+        $("#btn-save").show();
+        $("#form input").removeAttr("readonly");
+        $("#form select").removeAttr("disabled").parent().removeClass("disabled");
+    }
+    function cancel() {
+        $("#btn-cancel").hide();
+        $("#btn-edit").show();
+        $("#btn-save").hide();
+        $("#form input").attr("readonly", "readonly");
+        $("#form select").attr("disabled", "disabled").parent().addClass("disabled");
+        if(infoHasChanged() && confirm("是否撤销修改？")) {
+            fill(window.info);
+        }
+    }
+    function save() {
+        let userInfo = getInfo();
+        $.ajax({
+            "url": "{{ url('user/edit') }}",
+            "data": userInfo,
+            "dataType": "json",
+            "method": "POST",
+            "success": function(response) {
+                if(response.success) {
+                    alert("修改成功");
+                    location.reload();
+                }else{
+                    console.log(response.message);
                 }
             },
-            "json"
-        );
+            "error": function() {
+                console.log("请求失败");
+            },
+            "complete": function() {
+                closeLoader();
+            }
+        });
+        openLoader("正在保存...")
+    };
+    $(document).ready(function() {
+        (function() {
+            $('#gender').dropdown();
+            $.ajax({
+                "url": "{{ url('user/info') }}",
+                "dataType": "json",
+                "success": function(response) {
+                    if(response.success) {
+                        window.info.sid = response.data.sid || "";
+                        window.info.name = response.data.name || "";
+                        window.info.nickname = response.data.nickname || "";
+                        window.info.gender = response.data.gender || "UNKNOWN";
+                        window.info.email = response.data.email || "";
+                        fill(window.info);
+                    }else{
+                        console.log(response.message);
+                    }
+                },
+                "error": function() {
+                    console.log("请求失败");
+                },
+                "complete": function() {
+                    closeLoader();
+                }
+            });
+            openLoader("正在载入...");
+        })();
+        $("#form input").keyup(changeSaveButtonStatus);
+        $("#form select").change(changeSaveButtonStatus);
     });
 </script>
 @endpush
