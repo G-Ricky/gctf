dba917889ea2cf74b264a525101ee76663614a72
G-Ricky <284215049@qq.com>
2018-08-24 22:27:37 +0800

    新增数据库

---
+++ database/migrations/2018_05_12_062037_create_banks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 256)->comment('题库名');
+            $table->string('description', 1024)->comment('描述');
+            //$table->unsignedInteger('creator')->comment('创建人id');
+            $table->boolean('is_hidden')->comment('是否隐藏');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
