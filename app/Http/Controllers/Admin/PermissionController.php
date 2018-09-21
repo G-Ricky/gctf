1a67ddc317685f3f8e971fb2868670546cca848b
G-Ricky <284215049@qq.com>
2018-09-22 01:50:12 +0800

    添加加载用户权限页面

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Ability;
+use App\Models\Admin\Role;
+use Illuminate\Http\Request;
+use Silber\Bouncer\Bouncer;
 
 class PermissionController extends Controller
 {
-    public function index()
+    public function index($roleId)
     {
-        return view('admin.permission.index');
+        return view('admin.permission.index', [
+            'roleId' => $roleId
+        ]);
     }
 
-    public function list()
+    public function list($roleId)
     {
+        $roleModel = Role::where('id', '=', $roleId)->firstOrFail();
 
+        $abilities = $roleModel->abilities()->get()->toArray();
+        $abilities = array_column($abilities, 'id');
+
+        return [
+            'status'  => 200,
+            'success' => true,
+            'data'    => $abilities
+        ];
     }
 }
