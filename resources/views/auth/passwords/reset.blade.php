3a85c9177a51a1898708cca7a303ea8ff9459a74
Ricky <284215049@qq.com>
2018-10-03 15:37:38 +0800

    重置界面的ui

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,70 +1,95 @@
 @extends('layouts.app')
 
+@push('stylesheets')
+    <style>
+        html, body, #app {
+            height: 100%;
+        }
+        #app>.grid {
+            height: 100%;
+        }
+        .column {
+            max-width: 400px;
+        }
+
+        .ui.message ul {
+            padding-left: 20px;
+        }
+
+        .field,
+        .ui.message ul,
+        .ui.error.message {
+            text-align: left;
+        }
+        .ui.stacked.segment:after {
+            content: none;
+        }
+
+        .ui.error.message.visible {
+            display: block;
+        }
+    </style>
+@endpush
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="ui middle aligned center aligned grid">
+    <div class="column">
+        <h2 class="ui image header">
+            <img src="{{ asset('img') }}/logo.png" class="image">
+            <div class="content">{{ __('Reset Password') }}</div>
+        </h2>
+        <form class="ui large form" method="POST" action="{{ route('password.request') }}">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.request') }}">
-                        @csrf
+            <div class="ui stacked segment">
+                <div class="field">
+                    <div class="ui left icon input{{ $errors->has('email') ? ' error' : '' }}">
+                        <i class="user icon"></i>
+                        <input type="email" name="email" value="{{ $email or old('email') }}" placeholder="{{ __('E-Mail Address') }}" required autofocus>
+                    </div>
+                </div>
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+                <div class="field">
+                    <div class="ui left icon input{{ $errors->has('password') ? ' error' : '' }}">
+                        <i class="lock icon"></i>
+                        <input id="password" type="password" name="password" value="" placeholder="{{ __('Password') }}" required>
+                    </div>
+                </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <div class="field">
+                    <div class="ui left icon input{{ $errors->has('password_confirmation') ? ' error' : '' }}">
+                        <i class="lock icon"></i>
+                        <input id="password-confirm" type="password" name="password_confirmation" value="" placeholder="{{ __('Confirm Password') }}" required>
+                    </div>
+                </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email or old('email') }}" required autofocus>
+                <div class="field">
+                    <button type="submit" class="ui fluid large primary submit button">{{ __('Reset Password') }}</button>
+                </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="field">
+                    <button type="button" class="ui fluid large button" onclick="location.href='{{ url('login') }}'">{{ __('Cancel') }}</button>
                 </div>
             </div>
-        </div>
+            <div class="ui error message{{ count($errors) > 0 ? ' visible':'' }}">
+                @if ($errors->has('name'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
+                @if ($errors->has('password'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+                @if ($errors->has('password_confirmation'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                </span>
+                @endif
+            </div>
+        </form>
     </div>
 </div>
 @endsection
