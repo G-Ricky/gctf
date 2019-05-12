1747bc93997d2b2d225489e8cc106fbc48b41484
G-Ricky <284215049@qq.com>
2019-05-13 01:57:04 +0800

    优化前端错误处理

--- public/js/common/error.js
+++ public/js/common/error.js
@@ -28,3 +28,23 @@ function removeInputError(id) {
     $("#" + id).parents(".ui.form").removeClass("error");
     $("#" + id).parent(".field").removeClass("error").children(".ui.error.message").remove();
 }
+
+function handleError(jqXHR, textStatus, error) {
+    if(textStatus !== "parsererror" && jqXHR.status === 422) {
+        let messages = "";
+        let response = JSON.parse(jqXHR.responseText);
+        for(let key in response.errors) {
+            let error = response.errors[key];
+            for(let i = 0;i < error.length;++i) {
+                messages += "<p>" + error[i] + "</p>";
+            }
+        }
+
+        tip.error(messages);
+        return;
+    } else if(jqXHR.readyState === 0) {
+        tip.error("网络未连接！");
+    } else {
+        tip.error("未知错误！");
+    }
+}
